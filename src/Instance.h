9922f8467571e3cf9c7af090c0a6889d05418171
Cassiano Honorio <honoriocassiano@gmail.com>
2017-02-15 10:19:33 -0200

    Adição de documentação

--- src/Instance.h
+++ src/Instance.h
@@ -21,31 +21,65 @@ class Colony;
 
 class Instance {
 public:
+	/**
+	 * @param nItems Number of items
+	 * @param nKnapsacks Number of knapsacks
+	 * @param items List of items
+	 * @param capacities List of knapsacks
+	 */
 	Instance(std::size_t nItems, std::size_t nKnapsacks,
 			const std::vector<Item>& items, const std::vector<int>& capacities);
 
 	virtual ~Instance();
 
+	/**
+	 * @return List of items.
+	 */
 	const std::vector<Item>& getItems() const {
 		return items;
 	}
 
+	/**
+	 * @return List of capacities.
+	 */
 	const std::vector<int>& getCapacities() const {
 		return capacities;
 	}
 
+	/**
+	 * @return Number of items.
+	 */
 	std::size_t getNumItems() const {
 		return nItems;
 	}
 
+	/**
+	 * @return Number of knapsacks.
+	 */
 	std::size_t getNumKnapsacks() const {
 		return nKnapsacks;
 	}
 
 private:
+
+	/**
+	 * Number of items.
+	 */
 	std::size_t nItems;
+
+	/**
+	 * Number of knapsacks.
+	 */
 	std::size_t nKnapsacks;
+
+	/**
+	 * List of items.
+	 */
 	std::vector<Item> items;
+
+	/**
+	 * List of capaticies.
+	 */
 	std::vector<int> capacities;
 
 	friend class Solution;
